e051fc4e5c4e172ac75d805024991e62181364c0
reabetsweModise <reabetswemodise02@gmail.com>
2023-07-24 14:59:23 -0700

    lists

---
+++ history.c
@@ -0,0 +1,146 @@
+#include "shell.h"
+
+/**
+ * get_history_file -This Function gets the history file
+ * @info: parameter struct
+ *
+ * Return: allocated string containg history file
+ */
+
+	char *get_history_file(info_t *info)
+{
+	char *buf, *dir;
+
+	dir = _getenv(info, "HOME=");
+	if (!dir)
+		return (NULL);
+	buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
+	if (!buf)
+		return (NULL);
+	buf[0] = 0;
+	_strcpy(buf, dir);
+	_strcat(buf, "/");
+	_strcat(buf, HIST_FILE);
+	return (buf);
+}
+
+/**
+ * write_history -This Function creates a file, or appends to an existing file
+ * @info: the parameter struct
+ *
+ * Return: 1 on success, else -1
+ */
+int write_history(info_t *info)
+{
+	ssize_t file_descriptor;
+	char *filename = get_history_file(info);
+	list_t *node = NULL;
+
+	if (!filename)
+		return (-1);
+
+	file_descriptor = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
+	free(filename);
+	if (file_descriptor == -1)
+		return (-1);
+	for (node = info->history; node; node = node->next)
+	{
+		_putsfd(node->str, file_descriptor);
+		_putfd('\n', file_descriptor);
+	}
+	_putfd(BUF_FLUSH, file_descriptor);
+	close(file_descriptor);
+	return (1);
+}
+
+/**
+ * read_history -This Function reads history from file
+ * @info: the parameter struct
+ *
+ * Return: histcount
+ */
+int read_history(info_t *info)
+{
+	int i, last = 0, linecount = 0;
+	ssize_t file_descriptor, bytes_read, hist_fsize = 0;
+	struct stat st;
+	char *buf = NULL, *filename = get_history_file(info);
+
+	if (!filename)
+		return (0);
+
+	file_descriptor = open(filename, O_RDONLY);
+	free(filename);
+	if (file_descriptor == -1)
+		return (0);
+	if (!fstat(file_descriptor, &st))
+		hist_fsize = st.st_size;
+	if (hist_fsize < 2)
+		return (0);
+	buf = malloc(sizeof(char) * (hist_fsize + 1));
+	if (!buf)
+		return (0);
+	bytes_read = read(file_descriptor, buf, hist_fsize);
+	buf[hist_fsize] = 0;
+	if (bytes_read <= 0)
+		return (free(buf), 0);
+	close(fd);
+	for (i = 0; i < fsize; i++)
+		if (buf[i] == '\n')
+		{
+			buf[i] = 0;
+			build_history_list(info, buf + last, linecount++);
+			last = i + 1;
+		}
+	if (last != i)
+		build_history_list(info, buf + last, linecount++);
+	free(buf);
+	info->histcount = linecount;
+	while (info->histcount-- >= HIST_MAX)
+		delete_node_at_index(&(info->history), 0);
+	renumber_history(info);
+	return (info->histcount);
+}
+
+/**
+ * build_history_list -This Function adds entry to a history linked list
+ * @info:This parameter Structure containing potential arguments.
+ * Used to maintain
+ * @buf:This parameter buffer
+ * @linecount:This parameter the history linecount, histcount
+ *
+ * Return: Always 0
+ */
+int build_history_list(info_t *info, char *buf, int linecount)
+{
+	list_t *node = NULL;
+
+	if (info->history)
+		node = info->history;
+	add_node_end(&node, buf, linecount);
+
+	if (!info->history)
+		info->history = node;
+	return (0);
+}
+
+/**
+ * renumber_history -This Function renumbers the history linked
+ * list after changes
+ * @info:This parameter structure containing potential arguments.
+ * Used to maintain
+ *
+ * Return: the new histcount
+ */
+int renumber_history(info_t *info)
+{
+	list_t *node = info->history;
+	int i = 0;
+
+	while (node)
+	{
+		node->num = i++;
+		node = node->next;
+	}
+	return (info->histcount = i);
+}
---
+++ lists1.c
@@ -0,0 +1,122 @@
+#include "shell.h"
+
+/**
+ * list_len -This Function determines length of linked list
+ * @h:This parameter pointer to first node
+ *
+ * Return: size of list
+ */
+	size_t list_len(const list_t *h)
+{
+	size_t index = 0;
+
+	while (h)
+	{
+		h = h->next;
+		index++;
+	}
+	return (index);
+}
+
+/**
+ * list_to_strings -This Function returns an array of strings of the list->str
+ * @head:This parameter pointer to first node
+ *
+ * Return: array of strings
+ */
+char **list_to_strings(list_t *head)
+{
+	list_t *node = head;
+	size_t first_counter = list_len(head), second_counter;
+	char **strs;
+	char *str;
+
+	if (!head || !first_counter)
+		return (NULL);
+	 strs = malloc(sizeof(char *) * (first_counter + 1));
+	if (!strs)
+		return (NULL);
+	for (first_counter = 0; node; node = node->next, first_counter++)
+	{
+		str = malloc(_strlen(node->str) + 1);
+		if (!str)
+		{
+			for (second_counter = 0; second_counter < first_counter; second_counter++)
+				free(strs[second_counter]);
+			free(strs);
+			return (NULL);
+		}
+
+		str = _strcpy(str, node->str);
+		strs[current_pos] = str;
+	}
+	strs[i] = NULL;
+	return (strs);
+}
+
+
+/**
+ * print_list -This Function prints all elements of a list_t linked list
+ * @h:This parameter pointer to first node
+ *
+ * Return: size of list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t index = 0;
+
+	while (h)
+	{
+		_puts(convert_number(h->num, 10, 0));
+		_putchar(':');
+		_putchar(' ');
+		_puts(h->str ? h->str : "(nil)");
+		_puts("\n");
+		h = h->next;
+		index++;
+	}
+	return (index);
+}
+
+/**
+ * node_starts_with -This Function returns node whose string starts with prefix
+ * @node:This parameter pointer to list head
+ * @prefix:This parameter string to match
+ * @c: the next character after prefix to match
+ *
+ * Return: match node or null
+ */
+list_t *node_starts_with(list_t *node, char *prefix, char c)
+{
+	char *my_pointer = NULL;
+
+	while (node)
+	{
+		my_pointer = starts_with(node->str, prefix);
+		if (my_pointer && ((c == -1) || (*my_pointer == c)))
+			return (node);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * get_node_index -This Function gets the index of a node
+ * @head:This parameter pointer to list head
+ * @node:This parameter pointer to the node
+ *
+ * Return: index of node or -1
+ */
+ssize_t get_node_index(list_t *head, list_t *node)
+{
+	size_t loop_count = 0;
+
+	while (head)
+	{
+		if (head == node)
+			return (loop_count);
+		head = head->next;
+		loop_count++;
+	}
+	return (-1);
+}
